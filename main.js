5a737424be28c5cecc415d6ea96b55a4b69e8085
Ilya S <110722815+ils01@users.noreply.github.com>
2023-02-03 21:01:46 +0300

    feat: add toggle read status logic

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const bookList = document.querySelector("ul.library");
+/*
+    remove book 
+    change book read status
+    new book btn for menu to pop up
+*/
+
+const bookList = document.querySelector("ul.book-list");
 const form = document.querySelector("form");
 
 const myLibrary = [];
@@ -7,12 +13,59 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = `${read ? "read" : "not read yet"}`;
+  this.read = read;
 }
 
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
 };
+
+// add check for empty books
+function displayAllBooks() {
+  bookList.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const currBook = myLibrary[i];
+    const listItem = document.createElement("li");
+
+    for (key in currBook) {
+      if (key !== "info" && key !== "read") {
+        const para = document.createElement("p");
+        para.textContent = `${key}: ${currBook[key]}`;
+        listItem.appendChild(para);
+      }
+    }
+
+    const label = document.createElement(`label`);
+    label.setAttribute("for", "read");
+    label.textContent = `read: `;
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", "read");
+    checkbox.setAttribute("data-title", currBook.title);
+    listItem.append(label, checkbox);
+    const remove = document.createElement("button");
+    remove.textContent = "delete";
+    remove.setAttribute("type", "button");
+    remove.id = "remove";
+    listItem.appendChild(remove);
+
+    if (currBook.read) checkbox.checked = true;
+    checkbox.addEventListener("change", (event) => {
+      toggleReadStatus(event.target.getAttribute("data-title"));
+    });
+
+    bookList.appendChild(listItem);
+  }
+}
+
+function toggleReadStatus(dataTitle) {
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    if (myLibrary[i].title === dataTitle) {
+      if (myLibrary[i].read === true) myLibrary[i].read = false;
+      else myLibrary[i].read = true;
+    }
+  }
+}
 
 function addBookToLibrary() {
   const title = document.getElementById("title").value;
@@ -21,10 +74,8 @@ function addBookToLibrary() {
   const read = document.getElementById("read").checked;
 
   const book = new Book(title, author, pages, read);
-  const li = document.createElement("li");
-  li.textContent = book.info();
   myLibrary.push(book);
-  bookList.appendChild(li);
+  displayAllBooks();
 }
 
 form.addEventListener("submit", (event) => {
@@ -32,12 +83,3 @@ form.addEventListener("submit", (event) => {
   addBookToLibrary();
   form.reset();
 });
-
-// add check for empty books
-function displayAllBooks() {
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    const book = document.createElement("li");
-    book.textContent = myLibrary[i].info();
-    bookList.appendChild(book);
-  }
-}
