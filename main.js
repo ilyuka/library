8a0741a961940cf544c0e8f10b7fbde48ac8c542
My Name <theilyashulepov@gmail.com>
2023-07-30 15:59:32 +0300

    feat: replace obj constructors with classes

--- main.js
+++ main.js
@@ -11,114 +11,114 @@ const close = document.querySelector(".close");
 const notification = document.querySelector(".notification");
 
 close.addEventListener("click", () => {
-  addBookPopUpSection.style.display = "none";
+    addBookPopUpSection.style.display = "none";
 });
 
 add.addEventListener("click", () => {
-  addBookPopUpSection.style.display = "flex";
+    addBookPopUpSection.style.display = "flex";
 });
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    showInfo() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+    }
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-};
-
-// add check for empty books
 function displayAllBooks() {
-  bookList.innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    const currBook = myLibrary[i];
-    const listItem = document.createElement("li");
+    bookList.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        const currBook = myLibrary[i];
+        const listItem = document.createElement("li");
 
-    for (key in currBook) {
-      if (key !== "info" && key !== "read") {
-        const para = document.createElement("p");
-        para.textContent = `${key}: ${currBook[key]}`;
-        listItem.appendChild(para);
-      }
+        for (key in currBook) {
+            if (key !== "info" && key !== "read") {
+                const para = document.createElement("p");
+                para.textContent = `${key}: ${currBook[key]}`;
+                listItem.appendChild(para);
+            }
+        }
+
+        const div = document.createElement("div");
+        const label = document.createElement("label");
+        label.setAttribute("for", "read");
+        label.textContent = `read: `;
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("id", "read");
+        checkbox.setAttribute("data-title", currBook.title);
+        div.append(label, checkbox);
+        const remove = document.createElement("button");
+        remove.textContent = "delete";
+        remove.setAttribute("type", "button");
+        remove.style.display = "block";
+        remove.id = "remove";
+        listItem.append(div, remove);
+
+        if (currBook.read) checkbox.checked = true;
+        checkbox.addEventListener("change", (event) => {
+            toggleReadStatus(checkbox.getAttribute("data-title"));
+        });
+
+        remove.addEventListener("click", (event) => {
+            removeBook(checkbox.getAttribute("data-title"));
+            bookList.innerHTML = "";
+            displayAllBooks();
+        });
+
+        bookList.appendChild(listItem);
     }
-
-    const div = document.createElement("div");
-    const label = document.createElement("label");
-    label.setAttribute("for", "read");
-    label.textContent = `read: `;
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", "read");
-    checkbox.setAttribute("data-title", currBook.title);
-    div.append(label, checkbox);
-    const remove = document.createElement("button");
-    remove.textContent = "delete";
-    remove.setAttribute("type", "button");
-    remove.style.display = "block";
-    remove.id = "remove";
-    listItem.append(div, remove);
-
-    if (currBook.read) checkbox.checked = true;
-    checkbox.addEventListener("change", (event) => {
-      toggleReadStatus(checkbox.getAttribute("data-title"));
-    });
-
-    remove.addEventListener("click", (event) => {
-      removeBook(checkbox.getAttribute("data-title"));
-      bookList.innerHTML = "";
-      displayAllBooks();
-    });
-
-    bookList.appendChild(listItem);
-  }
 }
 
 function removeBook(dataTitle) {
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    if (myLibrary[i].title === dataTitle) {
-      myLibrary.splice(i, 1);
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        if (myLibrary[i].title === dataTitle) {
+            myLibrary.splice(i, 1);
+        }
     }
-  }
 }
 
 function toggleReadStatus(dataTitle) {
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    if (myLibrary[i].title === dataTitle) {
-      if (myLibrary[i].read === true) myLibrary[i].read = false;
-      else myLibrary[i].read = true;
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        if (myLibrary[i].title === dataTitle) {
+            if (myLibrary[i].read === true) myLibrary[i].read = false;
+            else myLibrary[i].read = true;
+        }
     }
-  }
 }
 
 function addBookToLibrary() {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").checked;
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
 
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    if (myLibrary[i].title === title) {
-      notification.textContent = "There is already book with this title";
-      setTimeout(() => {
-        notification.textContent = "";
-      }, 2000);
-      return;
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        if (myLibrary[i].title === title) {
+            notification.textContent = "There is already book with this title";
+            setTimeout(() => {
+                notification.textContent = "";
+            }, 2000);
+            return;
+        }
     }
-  }
 
-  addBookPopUpSection.style.display = "none";
+    addBookPopUpSection.style.display = "none";
 
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-  displayAllBooks();
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayAllBooks();
 }
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  addBookToLibrary();
-  form.reset();
+    event.preventDefault();
+    addBookToLibrary();
+    form.reset();
 });
